6faa368605643c26ce914a9775d79119bbdc01b7
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-04-27 11:12:46 +0530

    first commit

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+const Calculator = () => {
+  const [bmiValue, setBmiValue] = useState();
+  let { register, handleSubmit, reset } = useForm();
+  const handleValue = (data) => {
+    const height = parseFloat(data.height) / 100;
+    const weight = parseFloat(data.weight);
+    const bmiValue = weight / height ** 2;
+    setBmiValue(bmiValue.toFixed(2));
+    reset();
+  };
+  return (
+    <>
+      <div className="text-center ">
+        <img
+          src="/Images/premium_photo-1681400641919-d5d03f6c0720.avif"
+          alt="background-image"
+          className="h-[100vh] w-[100vw] absolute"
+        />
+        <h1 className="text-bold text-sm text-gray-600 pt-1 relative">
+          BMI Calculator
+        </h1>
+
+        <form action="" onSubmit={handleSubmit(handleValue)}>
+          <div className="flex gap-2 justify-center mt-2 relative">
+            <input
+              type="text"
+              {...register("height")}
+              placeholder="Enter Your Height"
+              className=" outline-none  text-[3px]  w-[80px] rounded-[2px] p-0.5 text-white bg-gray-600 "
+            />
+            <input
+              type="text"
+              {...register("weight")}
+              placeholder="Enter Your Weight"
+              className=" outline-none text-[3px]  w-[80px] rounded-[2px]  p-0.5 text-white bg-gray-600"
+            />
+          </div>
+          <button
+            type="submit"
+            className=" bg-blue-500 text-white p-0.5 rounded hover:bg-blue-600  mt-2  w-[150px] text-[5px] relative"
+          >
+            Check Your BMI
+          </button>
+        </form>
+
+        <div className="text-bold text-sm text-gray-600 relative">
+          {bmiValue}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Calculator;
