01817f3ee8daf6f6d3abbfca45bad661b3e2da7c
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-04-27 11:27:36 +0530

    first commit

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -23,29 +23,29 @@ const Calculator = () => {
         </h1>
 
         <form action="" onSubmit={handleSubmit(handleValue)}>
-          <div className="flex gap-2 justify-center mt-2 relative">
+          <div className="flex gap-5 justify-center mt-5 relative">
             <input
               type="text"
               {...register("height")}
               placeholder="Enter Your Height"
-              className=" outline-none  text-[5px]  w-1/2 rounded-[2px] p-1 text-white bg-gray-600 "
+              className=" outline-none  text-[10px]  w-[250px] rounded-[2px] p-3 text-white bg-gray-600 "
             />
             <input
               type="text"
               {...register("weight")}
               placeholder="Enter Your Weight"
-              className=" outline-none text-[3px]  w-1/2 rounded-[2px]  p-0.5 text-white bg-gray-600"
+              className=" outline-none text-[10px]  w-[250px] rounded-[2px]  p-3 text-white bg-gray-600"
             />
           </div>
           <button
             type="submit"
-            className=" bg-blue-500 text-white p-1 rounded hover:bg-blue-600  mt-2  w-[350px] text-[8px] relative"
+            className=" bg-blue-500 text-white p-1 rounded hover:bg-blue-600  mt-2  w-[350px] text-[10px] relative"
           >
             Check Your BMI
           </button>
         </form>
 
-        <div className="text-bold text-sm text-gray-600 relative">
+        <div className="text-bold text-3xl text-gray-600 relative">
           {bmiValue}
         </div>
       </div>
